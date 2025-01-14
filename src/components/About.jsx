712e9fbd03c4f789fbda95c004f2732fba59a4b7
imramesh222 <imrameshrawat@gmail.com>
2025-01-14 12:15:40 +0545

    changes to css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div>
       <div>
-        <img src="./images/ramesh.jpg" alt="" className='w-52 transform transistion-transform duration-300'/>
+        <img src="./images/ramesh.jpg" alt="" className='w-52 transform transistion-transform duration-300 hover:scale-105'/>
       </div>
     </div>
   )
