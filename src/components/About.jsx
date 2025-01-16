fdc0df232ca7cd6162919c8121123ffd31395f17
imramesh222 <imrameshrawat@gmail.com>
2025-01-16 23:00:29 +0545

    changes to css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
+    <>
       <div>
         <img src="./images/ramesh.jpg" alt="" className='w-52 transform transistion-transform duration-300 hover:scale-105'/>
       </div>
-    </div>
+    </>
   )
 }
 
