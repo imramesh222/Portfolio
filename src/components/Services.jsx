3310366865c365e150b98865c0cc634f986a280a
imramesh222 <imrameshrawat@gmail.com>
2025-01-18 20:02:52 +0545

    changes to css

---
+++ src/components/Services.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <>
+    <div className="text-center">
+      <h1 className='text-white text-2xl'>My Services</h1>
+    </div>
+      
+    </>
+  )
+}
+
+export default Services
